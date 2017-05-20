8689b563c51d09ef17c29e38df6260050c743e18
Julio Ribeiro <juliomobdeveloper@gmail.com>
2017-05-20 12:12:03 -0300

    Ajustes de Kotlin Plugin e Gradle 3.0

--- app/src/main/java/julio/com/br/kotlinbasicsoreilly/classes/CustomerKotlin.kt
+++ app/src/main/java/julio/com/br/kotlinbasicsoreilly/classes/CustomerKotlin.kt
@@ -3,11 +3,27 @@ package julio.com.br.kotlinbasicsoreilly.classes
 /**
  * Created by mira on 18/05/2017.
  */
-class CustomerKotlin(var id: Int, var name: String, var email: String) {
+data class CustomerKotlin(var id: Int, var name: String, var email: String){
+    override fun toString(): String {
+        return "{id: $id name: $name email: $email}"
+    }
+}
 
     fun main(args: Array<String>){
         val customer = CustomerKotlin(1, "Julio", "julioribeiro@mira.vc")
+        val customer2 = CustomerKotlin(2, "Julio", "julioribeiro@mira.vc")
         println(customer)
+        val customer3 = customer
+        val customer4 = customer.copy()
+        if(customer == customer2){
+            println("They are the same");
+        }
+
+        println(customer3.id)
+        println(customer4.id)
+        println(customer4.name)
+
+        val customer5 = customer.copy(12, email ="julio")
+        println(customer5)
     }
 
-}
