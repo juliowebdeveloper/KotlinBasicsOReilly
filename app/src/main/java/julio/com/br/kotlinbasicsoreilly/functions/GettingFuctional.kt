35ff824ec4a2241858b387dfa93d485f6845772a
Julio <juliowebdeveloper@hotmail.com>
2017-05-30 16:48:04 -0300

    Classes

--- app/src/main/java/julio/com/br/kotlinbasicsoreilly/functions/GettingFuctional.kt
+++ app/src/main/java/julio/com/br/kotlinbasicsoreilly/functions/GettingFuctional.kt
@@ -19,6 +19,21 @@ fun unaryOperation(x:Int,  op: (Int) -> Int){//Recebe como parametro uma functio
 
 }
 
+fun unaryOperation2(op: (Int) -> Int){}
+
+fun transaction(db: Database, code: () ->Unit){ //Function que recebe o database e um codigo a ser executado
+                //Executa o codigo em try e no finally commita a partir do database class
+    try{
+        code()
+    }finally {
+        db.commit()
+    }
+
+}
+
+class Database {
+    fun commit(){}
+}
 
 fun suma(x:Int, y:Int) = x + y
 
@@ -33,6 +48,21 @@ fun main(args: Array<String>) {
 
     unaryOperation(1,  {x -> x * x})
     unaryOperation(2, {it * it})//Caso só tenha um parametro na function, é possivel usar "it" para substitui-lo
+
+    unaryOperation2 {  //Parece uma keyword da linguagem
+        it * it
+    }
+
+
+    val db = Database()
+
+    transaction(db){
+        //Interact with database
+        //Recebe esse codigo e no finally faz o commit da classe database, acaba criando uma keyword transaction
+        //Micro - DSL são maneiras de se criar codigos que possuem semanticas mais significativas em torno do negocio
+    }
+
+    unaryOperation (3, fun(x:Int): Int {return x *x}) //Anonymous function - Com isso pode-se ter multiplos return points
 
 
 
--- app/src/main/java/julio/com/br/kotlinbasicsoreilly/nulls/NullSafety.kt
+++ app/src/main/java/julio/com/br/kotlinbasicsoreilly/nulls/NullSafety.kt
@@ -1,11 +1,23 @@
 package julio.com.br.kotlinbasicsoreilly.nulls
 
+import julio.com.br.kotlinbasicsoreilly.classes.CustomerJava
+
 /**
  * Created by Shido on 21/05/2017.
  */
+class Service{
+    fun evaluate(){}
+}
+class ServiceProvider(){
+fun createServices(): Service?{
+    return null
+}
+}
+
+
 fun main(args: Array<String>) {
 
-    val messages: String = "Message"
+    val messages: String = "Message" //Types não podem ser null
 
     var nullMessage: String? = null
     val inferredNull= null
@@ -13,5 +25,13 @@ fun main(args: Array<String>) {
     nullMessage = "Some Value"
     println(nullMessage.length) //Smart Cast to Kotlin String ver a classe Casting
 
+    val customerJava = CustomerJava()
+
+    customerJava?.email
+
+    val sp = createServiceProvider()
+    sp?.createServices()?.evaluate()
 
 }
+
+private fun createServiceProvider(): ServiceProvider? = ServiceProvider()
